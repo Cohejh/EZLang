d1f35af04281ae00309fa8d5493bac3208166499
COHEJH <87650074+Cohejh@users.noreply.github.com>
2024-08-07 12:56:02 +0200

    Update ezlang.py

--- ezlang.py
+++ ezlang.py
@@ -1,1 +1,24 @@
+# Lets begin by opening a demo file:
+import os
+code = open("test.ez", "r").readlines()
+prev_dir = os.getcwd()
+os.mkdir("test")
+os.chdir("test")
+def count_indents(s):
+    return len(s) - len(s.lstrip())
+output = open("test.py", "a")
+for line in code:
+	# We need to check if they are indenting correctly...
+	if (int(count_indents(line)) % 4) != 0:
+		os.chdir(prev_dir)
+		os.rmdir("test")
+		print("[Compiler Error]: Incorrect indentation! Use tabs or four spaces to indent.")
 
+	
+	# Get the command in a nice format
+	split_cmd = line.lstrip().split()
+	# We need to add handlers for every function
+
+	# Handler for say
+	if split_cmd[0] == "say":
+		
