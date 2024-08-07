70af2bef7a8ef4c789b2c0453083eb81eeb92d5e
COHEJH <87650074+Cohejh@users.noreply.github.com>
2024-08-07 13:17:45 +0200

    Update ezlang.py

--- ezlang.py
+++ ezlang.py
@@ -4,21 +4,33 @@ code = open("test.ez", "r").readlines()
 prev_dir = os.getcwd()
 os.mkdir("test")
 os.chdir("test")
-def count_indents(s):
-    return len(s) - len(s.lstrip())
+
+# Counts the number of indents, so that we can add them back in later...
+def count_indents(s:str) -> int:
+	return len(s) - len(s.lstrip())
+
+# Returns a copy of the line, without the "(Nx)" at the end for repeats.
+def fix_repeat(s:str) -> str:
+	if s[-2:] == "x)":
+		return s[:-5]
+	else:
+		return s
+
 output = open("test.py", "a")
 for line in code:
-	# We need to check if they are indenting correctly...
-	if (int(count_indents(line)) % 4) != 0:
-		os.chdir(prev_dir)
-		os.rmdir("test")
-		print("[Compiler Error]: Incorrect indentation! Use tabs or four spaces to indent.")
-
+	indents = count_indents(line) 
+	proposed_line = ""
 	
 	# Get the command in a nice format
+	fixed_cmd = line.lstrip()
+	norepeat_cmd = fix_repeat(fixed_cmd)
 	split_cmd = line.lstrip().split()
 	# We need to add handlers for every function
 
 	# Handler for say
 	if split_cmd[0] == "say":
-		
+		if split_cmd[1][1] == "\"":
+			proposed_line = (f"print({norepeat_cmd.removeprefix('say ').replace('\"','\\\"')})")
+
+
+	
