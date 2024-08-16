4aa60c9cc5436271dde0ffd6d610ee34c298ec68
COHEJH <jcohenkadosh@gmail.com>
2024-08-16 19:15:27 +0100

    Did some stuff

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # EZLang
-'''
-A simple programming language with natural syntax.
-Need to make it accessible
-problem is there are many available and popular coding languages in use right now that are just that...
-also what language should we use as base? as in english word? new language? code? pig latin?
 
-This is more of a programming exercise than making an actual useful language. (For now)
-In regards to syntax, i have written a demo script `test.ez` which is the script that we need to write a compiler for.
+Demo script in `test.ez`
+
+Our current task is to make it translate between EZLang and python, line by line.
+Before we can do that though, we have implemented one command, `say`. 
+What we need to do, is make it recursively work for nested brackets,
+that is, it detects brackets in code, and first translates what's inside the brackets.
+So, if we start with `say (say \"Hello\")`, the following steps happen
+1. `say (print("Hello"))`
+2. `print(print("Hello"))`
+This can be scaled up for larger brackets
+The way we will probably implement it is by extracting the brackets, and running them through the main convert_line command.
--- ezlang.py
+++ ezlang.py
@@ -1,39 +1,8 @@
-## Lets begin by opening a demo file:
-#import os
-#code = open("test.ez", "r").readlines()
-#prev_dir = os.getcwd()
-#os.mkdir("test")
-#os.chdir("test")
-#
-## Counts the number of indents, so that we can add them back in later...
-#def count_indents(s:str) -> int:
-#	return len(s) - len(s.lstrip())
-#
-## Returns a copy of the line, without the "(Nx)" at the end for repeats.
-#def fix_repeat(s:str) -> str:
-#	if s[-2:] == "x)":
-#		r = s[::-1]
-#		e = r.find("(")
-#		return s[:-(e + 1)]
-#	else:
-#		return s
-#
-#output = open("test.py", "a")
-#for line in code:
-#	indents = count_indents(line) 
-#	proposed_line = ""
-#	
-#	# Get the command in a nice format
-#	fixed_cmd = line.lstrip()
-#	norepeat_cmd = fix_repeat(fixed_cmd)
-#	split_cmd = line.lstrip().split()
-#	# We need to add handlers for every function
-#
-#	# Handler for say
-#	if split_cmd[0] == "say":
-#		if split_cmd[1][1] == "\"":
-#			proposed_line = (f"print({norepeat_cmd.removeprefix('say ').replace('\"','\\\"')})")
-#	
-#
-#	output.write(f"{' ' * indents}{proposed_line}")
-#
+line = "say (say (say \"Hello\"))"
+
+def parallel_nested_brackets():
+    pass
+
+def convert_line(s:str) -> str:
+    if s.startswith("say"):
+        return f"print({s[4:]})"
