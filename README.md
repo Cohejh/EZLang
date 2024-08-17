5c962954e8bdf7e8870da06b84713c1de1b3939f
COHEJH <jcohenkadosh@gmail.com>
2024-08-17 16:26:29 +0100

    Did some more stuff
    How vague

--- README.md
+++ README.md
@@ -3,13 +3,3 @@
 Demo script in `test.ez`
 
 Our current task is to make it translate between EZLang and python, line by line.
-Before we can do that though, we have implemented one command, `say`. 
-What we need to do, is make it recursively work for nested brackets,
-that is, it detects brackets in code, and first translates what's inside the brackets.
-So, if we start with `say (say \"Hello\")`, the following steps happen
-
-1. `say (print("Hello"))`
-2. `print(print("Hello"))`
-   
-This can be scaled up for larger brackets
-The way we will probably implement it is by extracting the bracketed text, and running them through the main convert_line command.
--- ezlang.py
+++ ezlang.py
@@ -3,10 +3,6 @@ line = "say (say (say \"Hello\"))"
 
 
 def compile(f:str):
-    os.mkdir(f.split(".")[0].capitalize())
-    c = open(f,"r")
-    code = c.readlines()
-    
     # Finally fixed and implemented nested function. Outputs an ugly mess of brackets, but it works...
     def return_bracket_indices(text:str) -> list[list[int]]:
         stack = []
@@ -24,7 +20,10 @@ def compile(f:str):
         else: 
             return None
 
-    def convert_line(s:str) -> str:
+    def count_indents(string:str) -> int:
+        return len(string) - len(string.lstrip(' '))
+    
+    def convert_line(s:str, indents:int) -> str:
         proposed_line = s
         if return_bracket_indices(s) != None:
             for bracket in return_bracket_indices(s):
@@ -33,7 +32,19 @@ def compile(f:str):
                 
         if proposed_line.startswith("say"):
             proposed_line = f"print({proposed_line[4:].strip()})"
+        if proposed_line.startswith("//"):
+            proposed_line = f"#{proposed_line.removeprefix('//')}"
             
         return proposed_line
+    
+    os.mkdir(f.split(".")[0].capitalize())
+    c = open(f,"r")
+    code = c.readlines()
+    c.close()
+    os.chdir(f.split(".")[0].capitalize())
+    output = open(f"{f.split('.')[0].lower()}.py", "a")
+    for line in code:
+        output.write(convert_line(line) + "\n")
             
-print(convert_line(line))
+#print(convert_line(line))
+compile("test.ez")
